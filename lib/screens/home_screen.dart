bea3b859f5a979bcefde34cc00de839e49a483bf
aedign <aedign@icloud.com>
2021-03-01 19:58:29 -0500

    Added working map to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,9 @@ import 'package:text_marks_the_spot_app/constants.dart';
 import 'package:text_marks_the_spot_app/create_textmark_screen.dart';
 import 'package:text_marks_the_spot_app/data/data_handling.dart';
 import 'package:text_marks_the_spot_app/screens/account_settings/account_screen.dart';
+import 'package:text_marks_the_spot_app/functionality/geolocation/geolocation.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String id = 'home_screen';
@@ -23,49 +26,59 @@ class HomeScreen extends StatelessWidget {
       ),
       backgroundColor: kAccentColor,
       body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/map_ph.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Padding(
-          padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
-          child: SafeArea(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    IconButton(
-                      icon: Icon(
-                        Icons.location_searching_sharp,
-                        size: 55.0,
-                        color: kPrimaryColor,
-                      ),
-                      onPressed: () {},
-                    ),
-                  ],
-                ),
-                Expanded(
-                  child: SizedBox(),
-                ),
-                CustomButton(
-                  color: kPrimaryColor,
-                  textColor: Colors.white,
-                  btnText: 'Create a Text Mark',
-                  fontSize: 22.5,
-                  onTap: () {
-                    showModalBottomSheet(
-                      context: context,
-                      builder: (BuildContext context) => CreateTextMark(),
-                    );
-                  },
-                ),
-              ],
+        child: Stack(
+            overflow: Overflow.visible,
+          children: [
+            FlutterMap(
+            options: MapOptions(
+              center: LatLng(51.5, -0.09),
+              zoom: 13.0,
             ),
+            layers: [
+              TileLayerOptions(
+                  urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  subdomains: ['a', 'b', 'c']
+              ),
+            ],
           ),
-        ),
+            Padding(
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.location_searching_sharp,
+                          size: 55.0,
+                          color: kPrimaryColor,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: SizedBox(),
+                  ),
+                  CustomButton(
+                    color: kPrimaryColor,
+                    textColor: Colors.white,
+                    btnText: 'Create a Text Mark',
+                    fontSize: 22.5,
+                    onTap: () {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) => CreateTextMark(),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),]
+      ),
       ),
     );
   }
